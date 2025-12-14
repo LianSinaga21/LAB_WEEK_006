84d90c93d6b340875eb09d5c897006798fe8b345
LianSinaga21 <parsauliansinaga17@gmail.com>
2025-12-14 12:10:46 +0700

    Commit No.1 - Adding a RecyclerView and Populating it

---
+++ app/src/main/java/com/example/lab_week_006/model/CatBreed.kt
@@ -0,0 +1,5 @@
+package com.example.lab_week_006.model
+
+enum class CatBreed {
+    AmericanCurl, BalineseJavanese, ExoticShorthair
+}
---
+++ app/src/main/java/com/example/lab_week_006/model/Gender.kt
@@ -0,0 +1,5 @@
+package com.example.lab_week_006.model
+
+enum class Gender {
+    Female, Male, Unknown
+}
