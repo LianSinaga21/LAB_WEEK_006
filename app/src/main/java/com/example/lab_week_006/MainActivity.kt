2d04e259a3b5660e1a04769145ab12eacb3fa640
LianSinaga21 <parsauliansinaga17@gmail.com>
2025-12-14 12:22:37 +0700

    Commit No.4 - Assignment

--- app/src/main/java/com/example/lab_week_006/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_006/MainActivity.kt
@@ -3,11 +3,10 @@ package com.example.lab_week_006
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lab_week_006.model.*
-import androidx.recyclerview.widget.ItemTouchHelper
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,33 +34,23 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
+        // Swipe to delete
         val itemTouchHelper = ItemTouchHelper(catAdapter.swipeToDeleteCallback)
         itemTouchHelper.attachToRecyclerView(recyclerView)
 
-
+        // ✅ MINIMAL 10 DATA (ASSIGNMENT)
         catAdapter.setData(
             listOf(
-                CatModel(
-                    Gender.Male,
-                    CatBreed.BalineseJavanese,
-                    "Fred",
-                    "Silent and deadly",
-                    "https://cdn2.thecatapi.com/images/7dj.jpg"
-                ),
-                CatModel(
-                    Gender.Female,
-                    CatBreed.ExoticShorthair,
-                    "Wilma",
-                    "Cuddly assassin",
-                    "https://cdn2.thecatapi.com/images/egv.jpg"
-                ),
-                CatModel(
-                    Gender.Unknown,
-                    CatBreed.AmericanCurl,
-                    "Curious George",
-                    "Award winning investigator",
-                    "https://cdn2.thecatapi.com/images/bar.jpg"
-                )
+                CatModel(Gender.Male, CatBreed.BalineseJavanese, "Fred", "Silent and deadly", "https://cdn2.thecatapi.com/images/7dj.jpg"),
+                CatModel(Gender.Female, CatBreed.ExoticShorthair, "Wilma", "Cuddly assassin", "https://cdn2.thecatapi.com/images/egv.jpg"),
+                CatModel(Gender.Unknown, CatBreed.AmericanCurl, "Curious George", "Award winning investigator", "https://cdn2.thecatapi.com/images/bar.jpg"),
+                CatModel(Gender.Male, CatBreed.AmericanCurl, "Oscar", "Lazy but smart", "https://cdn2.thecatapi.com/images/bpc.jpg"),
+                CatModel(Gender.Female, CatBreed.BalineseJavanese, "Luna", "Very playful", "https://cdn2.thecatapi.com/images/MTY3ODIyMQ.jpg"),
+                CatModel(Gender.Male, CatBreed.ExoticShorthair, "Milo", "Sleep all day", "https://cdn2.thecatapi.com/images/4g3.jpg"),
+                CatModel(Gender.Female, CatBreed.AmericanCurl, "Nala", "Always curious", "https://cdn2.thecatapi.com/images/9cc.jpg"),
+                CatModel(Gender.Male, CatBreed.BalineseJavanese, "Simba", "Brave and loud", "https://cdn2.thecatapi.com/images/cml.jpg"),
+                CatModel(Gender.Unknown, CatBreed.ExoticShorthair, "Shadow", "Quiet observer", "https://cdn2.thecatapi.com/images/1rd.jpg"),
+                CatModel(Gender.Female, CatBreed.AmericanCurl, "Bella", "Friendly cat", "https://cdn2.thecatapi.com/images/6qi.jpg")
             )
         )
     }
